c21eeadb0c10a3a737f464b0ea33eba778d3d2ae
Yousof Khaled <yk849754@gmail.com>
2023-10-25 14:24:30 +0300

    create project and add .gnore file

---
+++ M-Commerce-Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M-Commerce-Admin
+//
+//  Created by Admin on 25/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ M-Commerce-AdminUITests/M_Commerce_AdminUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  M_Commerce_AdminUITestsLaunchTests.swift
+//  M-Commerce-AdminUITests
+//
+//  Created by Admin on 25/10/2023.
+//
+
+import XCTest
+
+final class M_Commerce_AdminUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
