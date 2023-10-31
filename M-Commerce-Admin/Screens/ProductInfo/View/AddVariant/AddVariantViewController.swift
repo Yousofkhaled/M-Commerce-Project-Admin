119c7f6c5c8ecedfd06cc4192a46538ce63f5bb7
Yousof Khaled <yk849754@gmail.com>
2023-10-31 22:28:50 +0200

    add variant to api

--- M-Commerce-Admin/Screens/ProductInfo/View/AddVariant/AddVariantViewController.swift
+++ M-Commerce-Admin/Screens/ProductInfo/View/AddVariant/AddVariantViewController.swift
@@ -17,11 +17,30 @@ class AddVariantViewController: UIViewController {
     
     @IBOutlet weak var quantity_text_field: UITextField!
     
-    
+    var product_ID : Int = 0
     var add_variant_closure : (() -> Void) = {}
     
     @IBAction func Add_button_pressed(_ sender: Any) {
-        add_variant_closure()
+        
+        // api call here
+        // one function (data + quantity)
+        
+        // api1 (product_id, option1, option2, price) {handler (variant_id, quantity)}
+        // api2 (variant_id, quantity) { handler view_model.initialize {reload my vc} }
+        
+        var manager = NetworkServices()
+        
+        
+        
+        manager.addProductVariant(product_id: product_ID, option_1: size_text_field.text ?? "911", option_2: color_text_field.text ?? "cyan", price: price_text_field.text ?? "1234.00") { inventory_item_id in
+            
+            manager.editVariantQuantity(inventory_item_id: inventory_item_id ?? 0, new_quantity: Int(self.quantity_text_field.text ?? "0") ?? 0) {
+                self.add_variant_closure()
+            }
+            
+        }
+        
+        //add_variant_closure()
     }
     
     
